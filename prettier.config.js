60fd26e49812fe703e79f876e94b22cf6316779b
HamraJekeen <143161594+HamraJekeen@users.noreply.github.com>
2024-09-19 22:53:44 +0530

    Add files via upload

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,19 @@
-const config = {
-	useTabs: true,
-	singleQuote: true,
-	bracketSameLine: true,
-	arrowParens: 'always',
-};
-
-module.exports = config;
+module.exports = {
+  // These settings are duplicated in .editorconfig:
+  tabWidth: 2, // indent_size = 2
+  useTabs: false, // indent_style = space
+  endOfLine: 'lf', // end_of_line = lf
+  semi: false, // default: true
+  singleQuote: true, // default: false
+  printWidth: 100, // default: 80
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  overrides: [
+    {
+      files: '*.js',
+      options: {
+        parser: 'flow',
+      },
+    },
+  ],
+}
